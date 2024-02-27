d64622dd4305f834ed48b131bafc6b429606b779
YoonJunHyeon <qmdldos@gmail.com>
2024-02-27 17:22:08 +0900

    feat : Leader.py 생성, csv파일을 읽는다

--- csvLeader.py
+++ csvLeader.py
@@ -1,1 +1,12 @@
-print('hello')
+import csv
+
+
+
+csv_file_path = './netflix_titles.csv'
+with open(csv_file_path, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        # 각 행의 데이터 처리 작업 수행  
+        print(row) 
+    # 반복문 종료
+    # with 블록 종료 후 자동으로 파일이 닫힙니다.
